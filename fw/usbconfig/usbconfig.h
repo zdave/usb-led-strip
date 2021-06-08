40fb2835866e8311292392c7cc8c3feae0a39178
David Emett <dave@sp4m.net>
2021-06-09 00:13:22 +0100

    Drop redundant USB from device name

--- fw/usbconfig/usbconfig.h
+++ fw/usbconfig/usbconfig.h
@@ -238,8 +238,8 @@
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'U', 'S', 'B', ' ', 'L', 'E', 'D', ' ', 'S', 't', 'r', 'i', 'p'
-#define USB_CFG_DEVICE_NAME_LEN 13
+#define USB_CFG_DEVICE_NAME     'L', 'E', 'D', ' ', 'S', 't', 'r', 'i', 'p'
+#define USB_CFG_DEVICE_NAME_LEN 9
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
